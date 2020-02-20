cdb5ae0fa856180dc68f310e3461e46e44fe81c2
knowledge <knowledgerudboy37@gmail.com>
2020-02-20 14:32:05 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace String_Manipulation_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter Yourname");
+           
+            string name=Console.ReadLine();
+            Console.WriteLine("Enter Contact Numbers");
+            string contactNum =Console.ReadLine();
+
+            
+            if ((contactNum.Length==10  ))
+            {//string manipulations 
+                Console.WriteLine($"The name is: {name.Length} characters longs");
+                Console.WriteLine($"Index of a character is: {name.IndexOf("w")}");
+                Console.WriteLine($"Returning first four characters: {name.Substring(4, 3)}");
+                string newContact = contactNum.Remove(2);
+                Console.WriteLine($"Removed didgits in the conttact:{newContact}");
+                Console.WriteLine($"Your phone number with SA Code:+27{contactNum.Substring(1)}");
+               
+            }
+            else {
+                Console.WriteLine("invalid contact");
+            }
+            
+        }
+    }
+}
